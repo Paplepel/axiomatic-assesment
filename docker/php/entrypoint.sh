2febc4c23110cb83637469df0b5de56e19f08a1b
Adriaan van Niekerk <adriaan@fgx.co.za>
2026-04-22 10:00:35 +0200

    fix: entrypoint uses exec "$@" so CMD php-fpm is not double-passed

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Fix storage/cache permissions after bind-mount overwrites Dockerfile chown
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
-exec php-fpm "$@"
+exec "$@"
